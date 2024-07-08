12aa9629a74c7270f76247dc342559b76baed906
Carlos-Abachi18 <wilfriedabachi@cesag.edu.sn>
2024-07-08 16:13:27 +0000

    avancement sur le projet d'application

---
+++ application.py
@@ -0,0 +1,130 @@
+from fonction_class import *
+
+menu_list = range(1,4)
+menu_cust_list = range(1,7)
+menu_trans_list = range(1,5)
+
+menu = """ choisissez parmi les 3  operations suivantes : 
+          1 - gestion des clients
+          2 - gestion des transactions
+          3 - sortir """
+
+menu_customer = """choisissez parmi les 5  operations suivantes : 
+            1- afficher la liste des clients
+            2- ajouter un client
+            3- supprimer un client
+            4- modifier les informations d'un client
+            5- afficher le solde d'un client 
+            6- retour """
+
+menu_transaction = """choisissez parmi les 4 operations suivantes : 
+            1- afficher toutes les transactions
+            2- afficher les transactions d'un client
+            3- ajouter la transaction entre deux clients
+            4- modifier le solde d'un client """
+
+while True:
+    print(menu)
+    choice = input( "👉Quel est votre choix:  ")
+    while not (choice.isdigit() or int(choice) in menu_list): 
+        print ("        ⛔ entrer un choix correct entre 1 et 3 ⛔")
+        print(menu)
+        choice = input("quel est votre choix ?: ")
+
+    if int(choice ) == 1:
+        while True:
+            print(menu_customer)
+            choice_cust = input( "👉Quel est votre choix:  ")
+            while not (choice_cust.isdigit() or int(choice_cust) in menu_cust_list): 
+                print ("        ⛔ entrer un choix correct entre 1 et 5 ⛔")
+                print(menu_customer)
+                choice_cust = input("quel est votre choix ?: ")
+
+            if int(choice_cust) == 1:
+                if customers_list == []:
+                    print("     💠votre liste de client est vide 💠")
+                else:
+                    for cust in customers_list:
+                        customer_display(cust)
+
+            elif int(choice_cust) == 2:
+                customer_add()
+
+            elif int(choice_cust) == 3:
+                 customers_del =input("💠entrer le nom du client que vous souhaiter supprimer: ")
+                 indic_del = None
+                 for cust in customers_list:
+                    if customers_del == cust.nom:
+                        indic_del= cust 
+                        break
+                    
+                 if indic_del != None:
+                    customers_list.remove(indic_del)
+                    print("     💠client supprimé avec succes💠")
+                 else:
+                   print("     ⛔aucun client avec ce nom⛔")
+
+            elif int(choice_cust) == 4:
+                client_name = input("entrer le nom du client pour qui vous souhaiter modifier des informations: ")
+                indic_change = None
+                for cust in customers_list:
+                    if client_name == cust.nom:
+                        indic_change = cust 
+                        break
+                if indic_change != None:
+                   customer_change(indic_change)
+                else:
+                   print("     ⛔aucun client avec ce nom⛔")
+
+            elif int(choice_cust) == 5:
+                client_name = input("entrer le nom du client pour qui vous souhaiter afficher le solde : ")
+                indic = None
+                for cust in customers_list:
+                    if client_name == cust.nom:
+                        indic = cust 
+                        break
+                if indic != None:
+                    customer_solde(indic)
+                else:
+                    print("     ⛔aucun client avec ce nom⛔")
+
+            elif int(choice_cust) == 6:
+                break 
+             
+                
+    elif int(choice) == 2: 
+         while True:
+            print(menu_transaction)
+            choice_transac = input( "👉Quel est votre choix:  ")
+            while not (choice_transac.isdigit() or int(choice_transac) in menu_trans_list): 
+                print ("        ⛔ entrer un choix correct entre 1 et 4 ⛔")
+                print(menu_transaction)
+                choice_transac = input("quel est votre choix ?: ")
+
+            if int(choice_transac) == 1:
+                if transaction_list == []:
+                    print("     💠 aucune transaction enregistrée 💠")
+                else:
+                    for trans in transaction_list:
+                        transaction_view(trans)
+
+            elif int(choice_transac) == 2:
+                code_customer = input(" entrez le code du client concerné : ")
+                indic = None 
+                for tran in transaction_list:
+                    if  code_customer == tran.code_emmeteur or tran.code_recepteur:
+                        indic = tran
+                        continue
+                if indic != None:
+                     transaction_view(tran)
+                else:
+                    print("⛔aucune transaction ne corespond avec ce code client⛔ ")
+                
+
+            elif int(choice_transac) == 3:
+                transction_add()
+           
+            elif int(choice_transac) == 4:
+                break 
+    elif int(choice) == 3:
+        exit()
---
+++ fonction_class.py
@@ -0,0 +1,116 @@
+from gettext import translation
+
+
+customers_list = []
+transaction_list = []
+class Customers:
+    num_customers = 1
+    def __init__(self, p_code, p_nom, p_phone, p_address,p_email,p_solde):
+        self.code = p_code
+        self.nom  = p_nom
+        self.phone = p_phone
+        self.address = p_address
+        self.email = p_email
+        self.solde = p_solde 
+        Customers.num_customers = Customers.num_customers + 1
+
+
+
+def customer_add():
+
+    code = f"00{Customers.num_customers}"
+    name = input("entrer le nom du client: ")
+    phone =input("entrer le telephone du client: ")
+    address=input("entrer l'adress du client:  ")
+    mail  =input("entrer l'email du client: ")
+    solde =input("entrer le solde du client: ")
+    client = Customers(code,name,phone,address,mail,solde)
+    customers_list.append(client)
+    print( f"       ✅ le client {client.nom} a bien été ajouté ✅ "  )   
+    
+
+def customer_display(self):
+
+    print(f"""client_{self.code}     
+            code = {self.code} 
+            nom  = {self.nom} 
+       téléphone = {self.phone}
+          adress = {self.address} 
+            mail = {self.email} 
+           solde = {self.solde} """)
+    print(f"{ "-" *35}" )
+
+def customer_change(self):
+    change_list = range(1,6)
+    menu_change ="""quelle information souhaitez vous modifier ?:
+                1- le code
+                2- le nom
+                3- le téléphone
+                4- l'adresse 
+                5- l'email 
+                6- le solde """
+    print(menu_change)
+    change_choice = input("👉entrez votre choix :  ")
+    while not (change_choice.isdigit() or int(change_choice) in change_list): 
+        print ("        ⛔ entrer un choix correct entre 1 et 5 ⛔")
+        print(menu_change)
+        change_choice = input("quel est votre choix ?: ")
+    if int(change_choice) == 1:
+        new_code = input(" entrez le nouveau code du client ")
+        self.code = new_code
+        print("     ✅ changement effectué avec succès ✅ ")
+    elif int(change_choice) == 2:
+        new_name = input(" entrez le nouveau nom du client ")
+        self.name = new_name
+        print("     ✅ changement effectué avec succès ✅ ")
+    elif int(change_choice) == 3:
+         new_phone = input(" entrez le nouveau téléphone du client ")
+         self.phone = new_phone
+         print("    ✅ changement effectué avec succès ✅ ")
+    elif int(change_choice) == 4:
+         new_address = input(" entrez le nouvel adresse  du client ")
+         self.address = new_address
+         print("    ✅ changement effectué avec succès ✅ ")
+    elif int(change_choice) == 5:
+         new_email = input(" entrez le nouveau email du client ")
+         self.email = new_email
+         print("    ✅ changement effectué avec succès ✅ ")
+    elif int(change_choice) == 6:
+         new_solde = input(" entrez le nouveau solde du client ")
+         self.solde = new_solde
+         print("    ✅ changement effectué avec succès ✅ ")
+
+def customer_solde(self):
+    print(f"le solde de {self.nom} est de {self.solde}")
+
+class Transaction:
+     num_transaction = 1 
+     def __init__ (self, p_ref_paiement,p_code_emmeteur, p_code_recepteur, p_date_transaction,p_montant, p_canal):
+       self.ref = p_ref_paiement
+       self.code_emmeteur = p_code_emmeteur 
+       self.code_recepteur = p_code_recepteur
+       self.date = p_date_transaction
+       self.montant = p_montant
+       self.canal = p_canal
+       Transaction.num_transaction = Transaction.num_transaction + 1 
+
+def transaction_view(self) :
+    print(f"""transaction_{self.ref}     
+            ref_paiement = {self.ref} 
+            code_emmeteur= {self.code_emmeteur} 
+          code_recepteur = {self.code_recepteur}
+         date_transaction= {self.date} 
+                 montant = { self.montant} 
+                   canal = {self.canal} """)
+    print(f"{ "-" *35}" )
+
+def transction_add():
+    canal         =input("entrer le canal utilisé pour la transaction : ")
+    ref           = f"{canal}_00A{Transaction.num_transaction}"
+    code_emeteur  = input("entrer le code du client emeteur : ")
+    code_recepteur= input("entrer le code du client recepteur : ")
+    date_trans    =input("entrer la date de la transaction :  ")
+    montant       =input("entrer le montant de la transaction : ")
+    transaction   = Transaction(ref,code_emeteur,code_recepteur,date_trans,montant,canal)
+    transaction_list.append(transaction)
+    print( f"       ✅ la transaction  {transaction.ref} a bien été ajouté ✅ "  )   
